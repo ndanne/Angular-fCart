1046a178d3d377076665cfefc4c7e617c6074b0f
Nagaraju Danne <nagaraju.danne@gmail.com>
2016-10-12 19:28:38 +0530

    Update myExam.js

--- myExam.js
+++ myExam.js
@@ -1,130 +1,131 @@
+'use strict';
+
 angular.module('myExam', [])
-.controller('examQusController', function($scope) {
-	var pos = 0, test, question, choice, choices, chA, chB, chC = null, chD=null, correct = 0;
-	$scope.questions = [
-    {
-        "qusId": 1,
-        "question": "Are you ready for exam?",
-        "senior": null,
-        "options": [
-            "Yes",
-            "No"
-        ],
-        "QuestionType": "multipleRadio",
-        "choice": "A"
-    },
-    {
-        "qusId": 2,
-        "question": "What is 10 + 4?",
-        "senior": null,
-        "options": [
-            "12",
-            "14",
-            "16"
-        ],
-        "QuestionType": "multipleRadio",
-        "choice": "B"
-    },
-    {
-        "qusId": 2,
-        "question": "What is 20 - 9?",
-        "senior": null,
-        "options": [
-            "9",
-            "14",
-            "11",
-            "13"
-        ],
-        "QuestionType": "multipleRadio",
-        "choice": "C"
-    },
-    {
-        "qusId": 3,
-        "question": "What is 7 x 3?",
-        "senior": null,
-        "options": [
-            "12",
-            "21",
-            "24",
-            "36"
-        ],
-        "choice": "A"
-    },
-    {
-        "question": "What is 8 / 2?",
-        "senior": null,
-        "options": [
-            "2",
-            "8",
-            "4"
-        ],
-        "choice": "C"
-    },
-    {
-        "qusId": 4,
-        "question": "What is sq-root 12?",
-        "senior": "this shold be calculate like 12*12",
-        "options": [
-            "155",
-            "122",
-            "55",
-            "144"
-        ],
-        "QuestionType": "multipleRadio",
-        "choice": "A"
-    },
-    {
-        "qusId": 5,
-        "question": "What is LMC of 8, 6, 14?",
-        "senior": "2*4*3*7",
-        "options": [
-            "168",
-            "144",
-            "120",
-            "84"
-        ],
-        "QuestionType": "multipleRadio",
-        "choice": "A"
-    }
-];
-	$scope.currentPage = 0;
-	$scope.pageSize = 5;
-	 $scope.numberOfPages=function(){
-        return Math.ceil($scope.questions.length/$scope.pageSize);                
-    }
-	
-// $element === angular.element() === jQuery() === $()
-$scope.test_status = "Started with "+$scope.questions.length+" Questions";
-this.checkAnswer = function checkAnswer(num, name){	
-	//var choices = document.getElementsByName("choices"+num);
-	//var choices = angular.element('input[name=choices+num]:checked').val();
-	/*window.alert($scope.toBool(option.Selected));
-	for(var i=0; i<choices.length; i++){
-		if(choices[i].checked){
-			choice = choices[i].value;
+
+.controller('examQusController', function($scope, examData, $log, $document) {
+	console.log($scope);
+	//var pos = 0, test, question, choice, choices, correct = 0;		 
+		/*$scope.$watch('fname', function(v){
+		  $scope.id = v;
+		});*/
+
+		$scope.questions = [];
+		examData.getQuestion().success(function(data, status, headers, config){
+			//console.log(data); console.log(status); console.log(headers);
+				$scope.questions 	= data;				
+				$scope.currentPage 	= 0;
+				$scope.pageSize 	= 5;
+				$scope.numberOfPages= function(){
+			        return Math.ceil($scope.questions.length/$scope.pageSize);   
+			    }
+			    $scope.test_status = "Started with "+$scope.questions.length+" Questions";
+			}).error(function(data, status, headers, config){
+				$log.warn(data, status, headers(), config)
+			});
+		
+		$scope.onSelect = function (option, question) {
+			console.log($scope.questions);
+			console.log("question-->"+question+" &option-->"+option);
+			//var ans ='isAns'+question;
+			console.log(option);
+			//$scope.ans = true;
+			$scope.isCorrect = function (index) {
+		        var result = 'correct';
+		        angular.forEach($scope.questions[index].options, function (option, index, array) {
+		            if (helper.toBool(option.Selected) != option.IsAnswer) {
+		                result = 'wrong';
+		                return false;
+		            }
+		        });
+		        return result;
+    		};
 		}
-		choices[i].disabled = true;
-	}
 
-	/*if(choice == questions[num][5]){
-		_(optId).style.backgroundColor = "#9CBA7F";
-		correct++;
-	} else{
-		_(optId).style.backgroundColor = "#FF6666";
-		wrong++;
-	}*/
-}
-
-//$scope.test_status = "Total Question ";
-}).filter('startFrom', function() {
-    return function(input, start) {
+	    $scope.isAnswered = function (mindex, choices) {
+	        var answered = 'Not Answered';
+	         var result = 'correct';
+	        var ans = $scope.questions[mindex].answer;
+	        angular.forEach($scope.questions[mindex].options, function (element, index, array) {
+	        	console.log(index);
+	        	console.log(ans);
+	        	//console.log(angular.element(document).find('choices'+mindex));
+	            if (index == ans) {
+	            	result = 'correct';
+	                answered = 'Answered';
+	                return false;
+	            }else{
+	            	result = 'wrong';
+	            }
+	        });
+	        return answered;
+   		 };
+})
+.factory('examData', function($http){	
+	return {
+		getQuestion: function(){			
+			return $http({method:'GET', url:'examDataOO.json'});
+		}	
+	}			
+})
+.filter('startFrom', function() {
+    return function(input, start) {    	
+    	if (input === undefined || input === null || input.length === 0){
+    	 return [];
+    	}
         start = +start; //parse to int
         return input.slice(start);
     }
-});
-/*
-.factory('examData', function(){
-	var examQuestions  = ; 
-	return examQuestions;
-});*/
+})
+.directive('testDir', function(){
+	return {
+		priority: 0,
+		/*template: '<div></div>',*/
+		templateUrl: 'testdir.html',
+		replace: true,
+		transclude: false,
+		restrict: 'E',
+		controller: 'examQusController',
+		compile: function(scope, iElement, iAttrs, controller){
+			return {
+				pre: function preLink(scope, iElement, iAttrs, controller){
+					console.log(iElement);
+				},
+				post: function postLink(scope, iElement, iAttrs, controller){
+					console.log(iElement);
+				}
+			}
+		},
+	}
 
+})
+.directive('sideBar', function(){
+	return {
+		priority: 0,
+		/*template: '<div></div>',*/
+		templateUrl: 'sidebar.html',
+		replace: true,
+		transclude: false,
+		restrict: 'E',
+		scope: {},
+		/*require: '^?ngModel',*/
+		controller: function($scope, $element, $attrs){
+			console.log("Dir controller..");
+			$scope.namePr = "Nagaraju Danne";
+			$scope.desig = "Technology Lead";
+			$scope.pathimg = "nobody.jpg";
+		},
+		compile: function(scope, iElement, iAttrs, controller){
+			return {
+				pre: function preLink(scope, iElement, iAttrs, controller){
+					//console.log("compile Pree..");
+				},
+				post: function postLink(scope, iElement, iAttrs, controller){
+					//console.log("compile post...");
+				}
+			}
+		},
+		link: function postLink(scope, iElement, iAttrs, controller){
+			//console.log("link link.."); Karthi@12
+		}
+	}
+});
