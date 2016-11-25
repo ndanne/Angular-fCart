27db8e4379e6d40b4a34334296c24082e3f28f3d
Nagaraju Danne <nagaraju.danne@gmail.com>
2016-11-25 20:44:22 +0530

    Update gamesAreaApp.js

--- services/gamesAreaApp.js
+++ services/gamesAreaApp.js
@@ -2,41 +2,38 @@
     'use strict';
  
     angular
-	.module('gameArenaApp', ['ngRoute'])
+	.module('gameArenaApp', ['ngRoute', 'ngResource'])
 
-	.config(function($routeProvider, $locationProvider) {
-	  $routeProvider.when('/', {
-			    template: '<div>this is jsut testing</div>',
-			 //   controller: 'gameMainCtrl',
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	  $routeProvider
+	  .when('/games', {
+			templateUrl: 'GamesArena.html',
+			
+			controller: 'gameMainCtrl',
+			controllerAs: 'gameVm'
 	    })
-	    .when('/games/:gameId', {
-			     templateUrl: 'book.html',
-			    controller: 'BookController',
-	    }).otherwise("/");
-	     $locationProvider.html5Mode(true);
-	})
-	.controller('gameMainCtrl', ['gameData',  function gameMainCtrl(gameData) {
-		var vm =this;
-		vm.maintitle = "Game Arena";
-		vm.gamesArena = [];
-		//console.log(gameData);
-		/*vm.$route = $route;
-		vm.$location = $location;
-		vm.$routeParams = $routeParams;*/
-			gameData.getGames().success(function(data, status, headers, config){
-				console.log(data);
-				vm.gamesArena = data.GamesArena;	
-				vm.currentPage 	= 0;
-				vm.pageSize 	= 4;
-				vm.numberOfPages= function(){
-			        return Math.ceil(vm.gamesArena.length/vm.pageSize);   
-			    }
-			}).error(function(data, status, headers, config){
-				//$log.warn(data, status, headers(), config);
-				console.log(status);
-			});
+	  .when('/games/:game_id', {
+			templateUrl: 'GameDetail.html',			
+			controller: 'gameDetailCtrl',
+			controllerAs: 'vmGame'
+	    })
+	    .when('/newGame', {
+			    templateUrl: 'NewGame.html',
+			    controller: 'NewGameCtrl',
+			    controllerAs: 'newGm',
+			   /* resolve: {
+			    	game: function($route, gameData){
+			    		return gameData.getGames($route.current.pathParams.game_id).$promise;
+			    	}
+			    }*/
+	    })
+	    .otherwise({redirectTo:"/games"});
+	    //$locationProvider.html5Mode({ enabled: true, requireBase: false });
 	}])
 	
+	.controller('gameMainCtrl', gameMainCtrl)
+	.controller('NewGameCtrl', NewGameCtrl)
+	.controller('gameDetailCtrl', gameDetailCtrl)
 .filter('startFrom', function() {
     return function(input, start) {    	
     	if (input === undefined || input === null || input.length === 0){
@@ -46,46 +43,91 @@
         return input.slice(start);
     }
 })
-.factory('gameData', ['$http', function gameData($http){
+.factory('gameData', ['$resource', function gameData($resource){
+		var resource = $resource('http://localhost:3000/api/games/:game_id', {game_id: '@game_id'}, {"getAll": {method:'GET', isArray: true, params:{something: "foo"}}});
 		return {
-			getGames: function(){
-				return $http({method:'GET', url:'http://localhost:3000/api/games/'});
-				//return $http({method:'GET', url:'gamesArenaData.json'});
-			},
-			saveGame: function(){
-				return $http({method:'POST', url:'http://localhost:3000/api/games/'});
-			},			
-			getGame: function(id){
-				return $http({method:'GET', url:'http://localhost:3000/api/games/'});
-			},
-			updateGame: function(id){
-				return $http({method:'PUT', url:'http://localhost:3000/api/games/'});
-			},
-			deleteGame: function(id){
-				return $http({method:'DELETE', url:'http://localhost:3000/api/games/'});
-			}
-		}			
+			getGames: getGames,
+			saveGame: saveGame,			
+			getAllGames: getAllGames,
+			updateGame: updateGame,
+			deleteGame: deleteGame
+		}	
+		function getGames(gameId){
+			return resource.get({game_id:gameId});			
+		}
+		function saveGame(game){
+			return resource.save(game);
+		}
+		function getAllGames(){
+			return resource.query();
+		}
+		function updateGame(id){
+			return resource.put({game_id:id});
+		}
+		function deleteGame(id){
+				return $resource({method:'DELETE', url:'http://localhost:3000/api/games/'});
+		}
 	}]);
+	//.controller('gameMainCtrl', ['gameData',  ])
+	gameMainCtrl.$inject = ['gameData'];
+	gameDetailCtrl.$inject = ['gameData', '$routeParams', '$location'];
+	NewGameCtrl.$inject = ['$scope', 'gameData'];
+	function gameMainCtrl(gameData) {
+		var vm =this;
+		//vm.maintitle = "Game Arena";
+		vm.gamesArena = [];
+		//console.log(gameData);
+		/*vm.$route = $route;
+		vm.$location = $location;
+		*/
+			gameData.getGames()
+				.$promise.then(success)
+				.catch(erorr);
+			function success(games){
+				vm.gamesArena = games.GamesArena;				
+			}
+			function erorr(response){
+				console.log(response);
+			}
+			vm.currentPage 	= 0;
+			vm.pageSize 	= 4;
+			vm.numberOfPages= function(){
+		        return Math.ceil(vm.gamesArena.length/vm.pageSize);   
+		    }
+	}
+	function gameDetailCtrl(gameData, $routeParams, $location){
+		var vm =this;
+		vm.gamesArena = [];
+		vm.$location = $location;
+		vm.$routeParams = $routeParams;
 
+			 gameData.getGames($routeParams.game_id)
+				.$promise.then(success)
+				.catch(erorr);
+			function success(game){				
+				vm.gamesArena = game.GamesArena[0];
+				console.log(vm.gamesArena);		
+			}
+			function erorr(response){
+				console.log(response);
+			}
+	}
+	function NewGameCtrl($scope, gameData){
+		$scope.saveGame =  function(game, newGameForm){
+			if(newGameForm.$valid){
+				console.log("Form valid succefully");
+				gameData.saveGame(game)
+				.$promise.then(function(response){
+						console.log(response);
+						window.location ='/';
+					},function(response){
+						console.log(response);
+					})
+			}
+		};
+		$scope.cancelBtn = function(){
+			window.location ='#/newGame';
+		}
+	}
 
 })();
-
-
-/*
-.factory('gameData', ['$http', function gameData($http){	
-		return {
-			getGames: function(){
-				return $http({method:'GET', url:'gamesArenaData.json'});
-			},
-			saveGame: function(){
-				return $http({method:'POST', url:'gamesArenaData.json'});
-			},
-			getGame: function(id){
-				return $http({method:'GET', url:'gamesArenaData.json'});
-			},
-			deleteGame: function(id){
-				return $http({method:'Delete', url:'gamesArenaData.json'});
-			}
-		}			
-	}]);
-*/	
